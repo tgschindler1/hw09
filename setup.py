eefed105995cc801e7e0df7115033ac712b4ebcc
tgschindler1@gmail.com <tgschindler1@gmail.com>
2025-03-22 17:02:28 -0400

    set up

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='hw09',
+    version='0.1',
+    url='https://github.com/tgschindler1/hw09',
+    author='Thomas Schindler',
+    author_email='tgschindler1@gmail.com',
+    license='MIT',
+    packages=['booklover']
+)
